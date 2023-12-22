75a466b9ca9bd175175daa4824d7dcd0a9b604d4
BojkovaA <108904427+BojkovaA@users.noreply.github.com>
2023-12-22 16:29:19 +0100

    Commit message

--- Domasna3/tech prototype/wineries-master/wineries-master/src/main/java/com/example/wineries/bootstrap/DataInsert.java
+++ Domasna3/tech prototype/wineries-master/wineries-master/src/main/java/com/example/wineries/bootstrap/DataInsert.java
@@ -1,6 +1,7 @@
 package com.example.wineries.bootstrap;
 
 import com.example.wineries.model.Winery;
+import com.example.wineries.repository.UserRepository;
 import com.example.wineries.repository.WineryRepository;
 import org.springframework.stereotype.Component;
 
@@ -19,16 +20,16 @@ public class DataInsert {
 
         @PostConstruct
         public void init() {
+        wineries= new ArrayList<>();
             if (wineryRepository.count() == 0) {
-                wineryRepository.save(new Winery("Chateau Sopot", 41.777774, 21.774216, "Veles"));
-                wineryRepository.save(new Winery("Tristo Winery", 41.7888, 21.7911, "Veles"));
-                wineryRepository.save(new Winery("Kocev Family Wines", 41.7097, 21.7878, "Veles"));
-                wineryRepository.save(new Winery("Izvorno Winery", 41.6038, 21.6928, "Veles"));
-                wineryRepository.save(new Winery("Popova Kula", 41.4109967, 22.2261756, "Demir Kapija"));
-                wineryRepository.save(new Winery("Bovin Winery", 41.4943704, 22.0915153, "Negotino"));
+                wineries.add(new Winery("Chateau Sopot", 41.777774, 21.774216, "Veles"));
+                wineries.add(new Winery("Tristo Winery", 41.7888, 21.7911, "Veles"));
+                wineries.add(new Winery("Kocev Family Wines", 41.7097, 21.7878, "Veles"));
+                wineries.add(new Winery("Izvorno Winery", 41.6038, 21.6928, "Veles"));
+                wineries.add(new Winery("Popova Kula", 41.4109967, 22.2261756, "Demir Kapija"));
+                wineries.add(new Winery("Bovin Winery", 41.4943704, 22.0915153, "Negotino"));
             }
-
-            List<Winery> wineries = wineryRepository.findAll();
-        }
+            wineryRepository.saveAll(wineries);
+    }
 
 }
